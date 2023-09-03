e013c7504306f1552b8780420ab9af38a0cd201b
Mohamed El-Gammal <memoelgammal@outlook.com>
2023-09-03 10:23:58 +0300

    Done First Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecommerce';
+  Name = "";
+  Power = 0;
+  Img ="";
+  Id = 0;
+
+  Heros: {hId : number; hName: string; hPower: number; hImage: string}[] = [];
+  
+  AddHero() {
+    this.Heros.push({hId : this.Id, hName: this.Name, hPower: this.Power, hImage:this.Img});
+    this.Id ++;
+  }
+  DelHero(i:number){
+    this.Heros.splice(i,1);
+  }
 }
